23561881231a6df1582689a5fe3deb38947427af
Danilo Ribeiro <danriba@gmail.com>
2019-02-22 22:43:12 -0300

    Create dragon_ball.cpp

---
+++ dragon_ball.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+#define m 10
+
+struct guerreiro
+{
+	char nome[10];
+	char planeta[10];
+	int poder;
+};
+
+void inserir()
+{
+	
+}
+
+int busca(guerreiro *g, i)
+{
+	
+}
+
+void listar()
+{
+	
+}
+
+void deletar()
+{
+	
+}
+
+int h1(int k,int i) // sondagem linear
+{
+	return (k + i) % m;
+}
+
+int main()
+{
+	
+	return 0;
+}
